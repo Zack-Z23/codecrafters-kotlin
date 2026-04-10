02d66ea2331f9f0b64f8160a3f283030a0f46711
Zack-Z23 <zack.zachairidis@gmail.com>
2026-04-10 18:37:15 -0500

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -6,11 +6,14 @@ fun main(args: Array<String>) {
 
     // Uncomment the code below to pass the first stage
      var serverSocket = ServerSocket(6379)
-    //
+
     // // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // // ensures that we don't run into 'Address already in use' errors
      serverSocket.reuseAddress = true
-    
-     serverSocket.accept() // Wait for connection from client.
-    println("accepted new connection")
+
+
+    val client = serverSocket.accept()
+    val out = client.getOutputStream()
+    out.write("+PONG\r\n".toByteArray())
+    out.flush()
 }
