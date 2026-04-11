488eb28c36222131db746086827797ec85188ebe
Zack-Z23 <zack.zachairidis@gmail.com>
2026-04-11 00:07:28 -0500

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -15,10 +15,10 @@ fun main(args: Array<String>) {
 
     while (true) {
         val client = serverSocket.accept()
+        val input = client.getInputStream()
+        val command = parseCommand(input.bufferedReader())
+        val out = client.getOutputStream()
         thread {
-            val input = client.getInputStream()
-            val command = parseCommand(input.bufferedReader())
-            val out = client.getOutputStream()
             when (command[0].uppercase()) {
                 "PING" -> out.write("+PONG\r\n".toByteArray())
                 "ECHO" -> out.write("$${command[1].length}\r\n${command[1]}\r\n".toByteArray())
