7acf5e29fd6e09108521882f651248acebd4cdbd
Zack-Z23 <zack.zachairidis@gmail.com>
2026-04-12 22:55:49 -0500

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -12,28 +12,40 @@ fun main(args: Array<String>) {
     // // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // // ensures that we don't run into 'Address already in use' errors
      serverSocket.reuseAddress = true
-
+    val store = java.util.concurrent.ConcurrentHashMap<String, String>()
     while (true) {
         val client = serverSocket.accept()
         thread {
             val input = client.getInputStream()
             val out = client.getOutputStream()
+            val command = parseCommand(input.bufferedReader())
             while(true) {
-                val command = parseCommand(input.bufferedReader())
                 if (command == null) break
                 when (command[0].uppercase()) {
                     "PING" -> out.write("+PONG\r\n".toByteArray())
                     "ECHO" -> out.write("$${command[1].length}\r\n${command[1]}\r\n".toByteArray())
-                    "SET" -> out.write("+OK\r\n".toByteArray())
-                    "GET ${command[1]}" -> out.write("$${command[2].length}\r\n${command[2]}\r\n".toByteArray())
+                    "SET" -> {
+                        store.put(command[1], command[2])
+                        out.write("+OK\r\n".toByteArray())
+
+                    }
+
+                    "GET" -> {
+                        val value = store[command[1]]
+                        if(value == null) break
+                        out.write("$${value.length}\r\n${value}\r\n".toByteArray())
+
+                    }
+
                 }
                 out.flush()
             }
 
         }
-    }
-
+     }
 }
+
+
 
 fun parseCommand(reader : BufferedReader) : List<String> {
     val firstLine = reader.readLine()
