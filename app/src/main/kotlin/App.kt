1ffb62d0d9a001cf257514a1289e5c08500e6e16
Zack-Z23 <zack.zachairidis@gmail.com>
2026-04-14 22:05:28 -0500

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -41,8 +41,10 @@ fun main(args: Array<String>) {
                         val entry = store[command[1]]
                         val value = entry?.first
                         val expiry = entry?.second
-                        if( value == null || expiry == null) {
-                            out.write("$-1\r\n".toByteArray())
+                        if (expiry != null) {
+                            if( value == null || expiry <= System.currentTimeMillis()) {
+                                out.write("$-1\r\n".toByteArray())
+                            }
                         }
                         out.write("$${value?.length}\r\n${value}\r\n".toByteArray())
 
