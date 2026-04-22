a66979a3949e420c570680d53380b84c27eceddc
Zack-Z23 <zack.zachairidis@gmail.com>
2026-04-22 00:24:46 -0500

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -149,13 +149,11 @@ fun main(args: Array<String>) {
                         var i = 0
                         val list = listOflists[command[1]]!!
 
-                        out.write("*${list.size}\r\n".toByteArray())
+                        out.write("*${command[2].toInt()}\r\n".toByteArray())
                         while (i < command[2].toInt()) {
-                            out.write("*${command[2].toInt()}\r\n".toByteArray())
                             out.write("$${list[0].length}\r\n${list[0]}\r\n".toByteArray())
                             list.removeFirst()
                             i++
-
                         }
                     }
 
